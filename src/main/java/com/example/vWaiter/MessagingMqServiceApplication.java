69774912b1b6fa1bb55b0f03ce5af5db314be41e
presidonaycs <presidonaycs@gmail.com>
2022-08-25 20:47:38 +0100

    second update

--- src/main/java/com/example/vWaiter/MessagingMqServiceApplication.java
+++ src/main/java/com/example/vWaiter/MessagingMqServiceApplication.java
@@ -11,7 +11,7 @@ public class MessagingMqServiceApplication {
 
 	@GetMapping("/message")
 	public String message() {
-		return "Congratulations!!!";
+		return "Congratulations!!! you have set up your application";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingMqServiceApplication.class, args);
